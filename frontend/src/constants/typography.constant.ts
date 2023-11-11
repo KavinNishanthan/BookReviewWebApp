afaf9b47ee663d57f0481f50a45632ae4fa108e0
kavin <kavinnishanthan2004@gmail.com>
2023-11-11 12:20:44 +0530

    typo

--- frontend/src/constants/typography.constant.ts
+++ frontend/src/constants/typography.constant.ts
@@ -1,16 +1,16 @@
 const typographyConstant = {
   loginPage: {
     SIGN_IN_TEXT: 'Sign In',
-    SIGN_IN_SUB_TEXT: "Let's open your skill repository.",
+    SIGN_IN_SUB_TEXT: '',
     WELCOME_TEXT: 'Welcome Back!',
-    WELCOME_SUB_TEXT: 'Login and see your performance and keep your learning curve like no one.',
+    WELCOME_SUB_TEXT: '',
     FORGOT_PASSWORD_TEXT: 'Forgot Password?'
   },
   registerPage: {
     SIGN_UP_TEXT: 'Sign up for an account',
-    SIGN_UP_SUB_TEXT: 'Integrate, Record & Grow',
-    WELCOME_TEXT: 'Start your Journey with us.',
-    WELCOME_SUB_TEXT: 'Learn, Discover and Build your Skill Repository with us. The future belong to you'
+    SIGN_UP_SUB_TEXT: '',
+    WELCOME_TEXT: '',
+    WELCOME_SUB_TEXT: ''
   }
 };
 
